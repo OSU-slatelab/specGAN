75dbb6593b94dcd6d8ae857163ca2053096364ef
Deblin Bagchi <deblinbagchi@gmail.com>
2017-06-07 19:58:00 -0400

    we can train a feedforward neural network using this code

--- specGAN.py
+++ specGAN.py
@@ -14,8 +14,9 @@ import collections
 import math
 import time
 from data_io import read_kaldi_ark_from_scp
+from six.moves import xrange 
 
-data_base_dir = "/data/data2/scratch/bagchid/specGAN"
+data_base_dir = "/data/data2/scratch/bagchid/specGAN-tf_old"
 def read_mats(uid, offset, batch_size, file_name):
     #Read a buffer containing 10*batch_size+offset 
     #Returns a line number of the scp file
@@ -23,62 +24,50 @@ def read_mats(uid, offset, batch_size, file_name):
     ark_dict,uid = read_kaldi_ark_from_scp(uid, offset, batch_size, scp_fn, data_base_dir)
     return ark_dict,uid
 
-def create_batch(ark_dict, batch_number, batch_size):
-    def create_permutations(ids):
-        return np.random.permutation(ids)
-        
-    #idnums = [i for (i,j) in enumerate(ids)]
+def loss(predictions, labels):
+  mse = tf.reduce_mean(tf.squared_difference(predictions, labels))
+  return tf.reduce_mean(mse, name='mse_mean')
 
-    rperm = create_permutations(idnums)
-    sub_ids = [ids[rperm[i]] for i in xrange(batch_number*batch_size, (batch_number+1)*batch_size)]
-    mats = [ark_dict[i].T for i in sub_ids]
-    return mats    
+def create_generator(generator_inputs, phase):
+    # Hidden 1
+    with tf.variable_scope('hidden1'):
+        weight = tf.get_variable("weight", [257, 1024], dtype=tf.float32, initializer = tf.random_normal_initializer(0,1))
+        bias = tf.get_variable("bias", [1024])
+        linear = tf.matmul(generator_inputs, weight) + bias
+        bn = tf.contrib.layers.batch_norm(linear,center=True, scale=True,is_training=phase)
+        hidden1 = tf.nn.relu(bn)
+    # Hidden 2
+    with tf.variable_scope('hidden2'):
+        weight = tf.get_variable("weight", [1024, 1024], dtype=tf.float32, initializer = tf.random_normal_initializer(0,1))
+        bias = tf.get_variable("bias", [1024])
+        linear = tf.matmul(hidden1, weight) + bias
+        bn = tf.contrib.layers.batch_norm(linear,center=True, scale=True,is_training=phase)
+        hidden2 = tf.nn.relu(bn)
 
-def create_model():
-    def create_generator(generator_inputs):
-        layers = []
-        with tf.variable_scope("gen_layer_1"):
-            output = dense(generator_inputs)
-            layers.append(output)
-            for _ in range(nlayers-1):
-                with tf.variable_scope("gen_layer_%d" % (len(layers) + 1)):
-                    rectified = dense(layers[-1])
-                    output = batchnorm(convolved)
-                    layers.append(output)
-        return layers[-1] 
+    # Linear
+    with tf.variable_scope('linear'):
+        weight = tf.get_variable("weight", [1024, 257], dtype=tf.float32, initializer = tf.random_normal_initializer(0,1))
+        bias = tf.get_variable("bias", [257])
+        linear = tf.matmul(hidden2, weight) + bias
+        bn = tf.contrib.layers.batch_norm(linear,center=True, scale=True,is_training=phase)
+    return bn
+    
 
-    def create_discriminator(discrim_inputs, discrim_targets):
-        n_layers = 6
-        layers = []
-        with tf.variable_scope("disc_layer_1"):
-            output = dense(generator_inputs)
-            layers.append(output)
-            for _ in range(nlayers-1):
-                with tf.variable_scope("disc_layer_%d" % (len(layers) + 1)):
-                    rectified = dense(layers[-1])
-                    output = batchnorm(convolved)
-                    layers.append(output)
-        return layers[-1]
+def training(loss, initial_learning_rate, num_steps_per_decay, decay_rate, max_global_norm=5.0):
+    trainables = tf.trainable_variables()
+    grads = tf.gradients(loss,trainables)
+    grads, _ = tf.clip_by_global_norm(grads, clip_norm=max_global_norm)
+    grad_var_pairs = zip(grads, trainables)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    learning_rate = tf.train.exponential_decay(
+            initial_learning_rate, global_step, num_steps_per_decay,
+            decay_rate, staircase=True)
+    tf.summary.scalar('loss', loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    train_op = optimizer.apply_gradients(grad_var_pairs, global_step=global_step)
+    return train_op
 
-    with tf.variable_scope("generator") as scope:
-        out_channels = int(targets.get_shape()[-1])
-        outputs = create_generator(inputs, out_channels)
-
-    with tf.name_scope("discriminator_real"):
-        with tf.variable_scope("discriminator"):
-            predict_real = create_discriminator(inputs, targets)
-
-    with tf.name_scope("discriminator_fake"):
-        with tf.variable_scope("discriminator", reuse=True):
-            predict_fake = create_discriminator(inputs, outputs)
-    with tf.name_scope("discriminator_loss"):
-        discrim_loss = tf.reduce_mean(-(tf.log(predict_real + EPS) + tf.log(1 - predict_fake + EPS)))
-
-    with tf.name_scope("generator_loss"):
-        gen_loss_GAN = tf.reduce_mean(-tf.log(predict_fake + EPS))
-        gen_loss_L1 = tf.reduce_mean(tf.abs(targets - outputs))
-        gen_loss = gen_loss_GAN * a.gan_weight + gen_loss_L1 * a.l1_weight
-
+    
 def fill_feed_dict(noisy_pl, clean_pl, config):
     batch_index = config['batch_index']
     batch_size = config['batch_size']
@@ -129,19 +118,20 @@ def fill_feed_dict(noisy_pl, clean_pl, config):
         
 
     
+    
 def placeholder_inputs(batch_size):
-    noisy_placeholder = tf.placeholder(tf.float32, shape=(batch_size,257))
-    clean_placeholder = tf.placeholder(tf.float32, shape=(batch_size,257))
+    noisy_placeholder = tf.placeholder(tf.float32, shape=(None,257))
+    clean_placeholder = tf.placeholder(tf.float32, shape=(None,257))
     return noisy_placeholder, clean_placeholder
 
-def main():
+def do_eval():
     batch_size=100000
     batch_index = 0
     offset_frames_noisy = np.array([], dtype=np.float32).reshape(0,257)
     offset_frames_clean = np.array([], dtype=np.float32).reshape(0,257)
     frame_buffer_clean = np.array([], dtype=np.float32)
     frame_buffer_noisy = np.array([], dtype=np.float32)
-
+    
     config = {'batch_size':batch_size, 'batch_index':0, 'uid':0, 'offset':0, 'offset_frames_noisy':offset_frames_noisy, 'offset_frames_clean':offset_frames_clean, 'frame_buffer_clean':frame_buffer_clean, 'frame_buffer_noisy':frame_buffer_noisy}
 
     noisy_pl, clean_pl = placeholder_inputs(batch_size)
@@ -150,15 +140,78 @@ def main():
         if feed_dict[noisy_pl].shape[0]<(batch_size*10):
             break
 
+    true_count = 0  # Counts the number of correct predictions.
+    while(True):
+        feed_dict = fill_feed_dict(noisy_pl, clean_pl)
+        true_count += sess.run(eval_correct, feed_dict=feed_dict)
+    precision = float(true_count) / num_examples
+    print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
+        (num_examples, true_count, precision))
+
+
+def run_training():
+    batch_size=1024
+    batch_index = 0
+    offset_frames_noisy = np.array([], dtype=np.float32).reshape(0,257)
+    offset_frames_clean = np.array([], dtype=np.float32).reshape(0,257)
+    frame_buffer_clean = np.array([], dtype=np.float32)
+    frame_buffer_noisy = np.array([], dtype=np.float32)
+    phase = True
+    config = {'batch_size':batch_size, 'batch_index':0, 'uid':0, 'offset':0, 'offset_frames_noisy':offset_frames_noisy, 'offset_frames_clean':offset_frames_clean, 'frame_buffer_clean':frame_buffer_clean, 'frame_buffer_noisy':frame_buffer_noisy}
+
+    noisy_pl, clean_pl = placeholder_inputs(batch_size)
+    tot_loss_epoch = 0
+    avg_loss_epoch = 0
+    with tf.Graph().as_default():
+        noisy_pl, clean_pl = placeholder_inputs(batch_size)
+        predictions = create_generator(noisy_pl, phase)
+        loss_val = loss(predictions, clean_pl)
+        train_op = training(loss_val,0.01,2,0.5)
+        summary = tf.summary.merge_all()
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+        sess = tf.Session()
+        summary_writer = tf.summary.FileWriter("log", sess.graph)
+
+        sess.run(init)
+        start_time = time.time()
+        for step in xrange(5312*10):
+            feed_dict, config = fill_feed_dict(noisy_pl, clean_pl, config)
+            if feed_dict[noisy_pl].shape[0]<batch_size:
+                batch_index = 0
+                offset_frames_noisy = np.array([], dtype=np.float32).reshape(0,257)
+                offset_frames_clean = np.array([], dtype=np.float32).reshape(0,257)
+                frame_buffer_clean = np.array([], dtype=np.float32)
+                frame_buffer_noisy = np.array([], dtype=np.float32)
+
+                config = {'batch_size':batch_size, 'batch_index':0, 'uid':0, 'offset':0, 'offset_frames_noisy':offset_frames_noisy, 'offset_frames_clean':offset_frames_clean, 'frame_buffer_clean':frame_buffer_clean, 'frame_buffer_noisy':frame_buffer_noisy}
 
 
 
+            _, loss_value = sess.run([train_op, loss_val], feed_dict=feed_dict)
+            tot_loss_epoch += loss_value
+
+            if (step+1) % 5312 == 0:
+                avg_loss_epoch = tot_loss_epoch/5312
+                tot_loss_epoch = 0   
+                duration = time.time() - start_time
+                start_time = time.time()
+                print ('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+                summary_str = sess.run(summary, feed_dict=feed_dict)
+                summary_writer.add_summary(summary_str, step)
+                summary_writer.flush()
+
+            #print ('Eval step:')
+ 
+            #do_eval(sess,
+                 #   eval_correct,
+                 #   noisy_pl,
+                 #   clean_pl)
 
 
-        
 
-
-
+def main():
+    run_training()
     
 if __name__=='__main__':
     main()    
